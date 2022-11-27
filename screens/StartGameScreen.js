e37ebde76bacd88aa4006623c294de3fc2c14b77
kakha <kakha.gujejiani@singular.uk>
2022-11-27 14:01:32 +0400

    button layouts modified

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,8 +6,11 @@ import PrimaryButton from '../components/PrimaryButton'
 function StartGameScreen(){
  return <View style={styles.inputContainer} >
     <TextInput style={styles.numberInput} maxLength={2} keyboardType='number-pad' autoCapitalize='none' autoCorrect={false} />
-    <PrimaryButton >Reset</PrimaryButton>
-    <PrimaryButton >Confirm</PrimaryButton>
+    <View style={styles.buttonsContainer}>
+   <View style={styles.buttonContainer} ><PrimaryButton >Reset</PrimaryButton></View>
+   <View style={styles.buttonContainer} ><PrimaryButton >Confirm</PrimaryButton></View>
+    </View>
+   
  </View>
 }
 
@@ -15,7 +18,8 @@ function StartGameScreen(){
 
 const styles = StyleSheet.create({
     inputContainer: {
-        
+        justifyContent: 'center',
+        alignItems: 'center',
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
@@ -39,6 +43,12 @@ const styles = StyleSheet.create({
       fontWeight: 'bold',
       width: 50,
       textAlign: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer: {
+        flex: 1
     }
 })
 // elevation for android
