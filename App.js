ba7767401604f5e4b9e61c55d3d8b167819fb766
kakha <kakha.gujejiani@singular.uk>
2022-12-03 13:07:59 +0400

    imageBackground added

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import {LinearGradient} from 'expo-linear-gradient'
+
 export default function App() {
   return (
-  <LinearGradient colors={['#ffb62f', '#72063c']} style={styles.rootScreen}><StartGameScreen /></LinearGradient>
+  <LinearGradient colors={['#ffb62f', '#72063c']}  style={styles.rootScreen}><ImageBackground style={styles.rootScreen} imageStyle={styles.backgroundImage} source={require('./assets/images/background.png')} resizeMode='cover'  ><StartGameScreen /></ImageBackground></LinearGradient>
   );
 }
 
@@ -18,5 +19,8 @@ const styles = StyleSheet.create({
   rootScreen:{
     flex: 1,
    
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
